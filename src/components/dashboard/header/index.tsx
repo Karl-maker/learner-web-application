e3ec1d6544dc6f08f4b64310d8ba31a9bdacf60f
Joshua Gonzales <122066304+TheXasl@users.noreply.github.com>
2024-02-19 14:33:07 -0400

    style: Header minor styling adjustment

--- src/components/dashboard/header/index.tsx
+++ src/components/dashboard/header/index.tsx
@@ -1,12 +1,14 @@
 export type HeaderBarParams = {
-    name: string;
-    profile_picture?: string;
-    isLoggedIn: boolean;
-}
+  name: string;
+  profile_picture?: string;
+  isLoggedIn: boolean;
+};
 
 export default function HeaderBar(params: HeaderBarParams) {
-    return <header>
-        <p>{params.name}</p>
-        {!params.isLoggedIn && <button>Login</button>}
+  return (
+    <header className="py-4 bg-primary text-2xl text-white">
+      <p>{params.name}</p>
+      {!params.isLoggedIn && <button>Login</button>}
     </header>
+  );
 }
